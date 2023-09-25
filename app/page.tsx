774a8806ce1cdb24fd483fec6001a9c95c259558
Edwin Chia <starlest@gmail.com>
2023-09-25 13:48:23 +0700

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,109 @@
-import Image from 'next/image'
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Input } from "@/components/ui/input"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { useState } from "react"
+
 
 export default function Home() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [language, setLanguage] = useState("");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="container h-screen w-screen bg-gray-300 flex flex-col gap-y-5 items-center justify-center text-center">
+      <div className="header">
+        <h1 className="font-bold text-2xl">Edwin & Yumico</h1>
+        <h2 className="font-muted">Wedding Invitation Text Generator</h2>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Input onChange={e => setFirstName(e.target.value)} placeholder="First Name" className="w-1/3" />
+      <Input onChange={e => setLastName(e.target.value)} placeholder="Last Name" className="w-1/3" />
+      <Select onValueChange={setLanguage}>
+        <SelectTrigger className="w-1/8">
+          <SelectValue placeholder="Language" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="eng">English</SelectItem>
+          <SelectItem value="id">Bahasa Indonesia</SelectItem>
+        </SelectContent>
+      </Select>
+      <AlertDialog>
+        <AlertDialogTrigger disabled={firstName == '' || lastName == '' || language == ''} className={`bg-primary text-white rounded-sm p-2 ${firstName == '' || lastName == '' || language == '' ? "opacity-50" : "opacity-100"}`}>Generate</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Wedding Invitation Text</AlertDialogTitle>
+            <AlertDialogDescription>
+              {language == "eng" &&
+                <>
+                  Dear {firstName} {lastName},
+                  <br />
+                  <br />
+                  We're overjoyed to share some wonderful news with you! Love has filled our hearts, and we would be truly honored to have you join us in celebrating our union in matrimony.
+                  <br />
+                  <br />
+                  You're Cordially Invited to Our Wedding! Please visit our wedding website for all the details:
+                  <br />
+                  https://kondangankuy.com/EdwinYumicowedding/?to={firstName}+{lastName}
+                  <br />
+                  <br />
+                  Your love and blessings mean the world to us, and we can't wait to see your radiant smile on our special day. We eagerly anticipate sharing this joyous occasion with you, surrounded by our beloved family and friends.
+                  <br />
+                  <br />
+                  With heartfelt love and boundless excitement,
+                  <br />
+                  Edwin & Yumico
+                </>
+              }
+              {
+                language == "id" &&
+                <>
+                  Kepada {firstName} {lastName},
+                  <br />
+                  <br />
+                  Kami sangat senang untuk berbagi kabar bahagia dengan Anda! Cinta telah memenuhi hati kami, dan kami akan sangat senang jika Anda bersedia berbagi kebahagiaan kami saat kami bersatu dalam pernikahan.
+                  <br />
+                  <br />
+                  Anda Diundang ke Pernikahan Kami! Silakan kunjungi situs web pernikahan kami untuk seluruh detail acara:
+                  <br />
+                  https://kondangankuy.com/EdwinYumicowedding/?to={firstName}+{lastName}
+                  <br />
+                  <br />
+                  Kami tak sabar untuk berbagi momen kebahagiaan ini bersama Anda, dikelilingi oleh keluarga dan sahabat.
+                  <br />
+                  <br />
+                  Dengan cinta dan kebahagiaan,
+                  <br />
+                  Edwin & Yumico
+                </>
+              }
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction>Done</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </main>
   )
 }
